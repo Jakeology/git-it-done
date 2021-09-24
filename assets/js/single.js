108da14245e859f1fa2348fbcbee6a171d3b8d9e
Jake <xxjakexx14@gmail.com>
2021-09-23 20:41:54 -0400

    added user can click on repos to view issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -12,11 +13,24 @@ var getRepoIssues = function (repo) {
 
         // check if api has paginated issues
         if (response.headers.get("Link")) {
-            displayWarning(repo);
+          displayWarning(repo);
         }
       });
+    } else {
+      document.location.replace("./index.html");
     }
   });
+};
+
+var getRepoName = function () {
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  } else {
+    document.location.replace("./index.html");
+  }
 };
 
 var displayIssues = function (issues) {
@@ -68,4 +82,4 @@ var displayWarning = function (repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
